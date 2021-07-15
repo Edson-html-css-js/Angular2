1b0b0469100d427cef2da0a0dbef7bc7fb3081f0
Edson-html-css-js <ed_ls@outlook.com>
2021-07-15 10:26:49 -0300

    Angular2-curso

---
+++ hello-typescript/main.js
@@ -0,0 +1,10 @@
+var minhavar = 'minha varialvel';
+function minhafunc(x, y) {
+    return x + y;
+}
+var num = 2;
+var pi = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function () {
+    return valor * 2;
+});
---
+++ hello-typescript/main.ts
@@ -0,0 +1,14 @@
+var minhavar = 'minha varialvel';
+
+function minhafunc(x ,y) {
+    return x + y;
+}
+
+let num = 2;
+
+const pi = 3.14;
+
+var numeros = [ 1,2,3]
+numeros.map(function(){
+    return valor * 2;
+});
